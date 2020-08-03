f4e2b11596b1ebf18e6764ba06f7305f54a56492
Nirav Patel <niravjdn@gmail.com>
2020-08-03 00:58:01 -0400

    Create table.js

---
+++ table.js
@@ -0,0 +1,90 @@
+var mydata = [{ "institution": "215920", "civicAddressMissingCount": 0.0, "totalRequests": 0.0, "cityMissingCount": 0.0, "provinceMissingCount": 0.0, "countryMissingCount": 0.0, "postalCodeMissingCount": 0.0, "partialKYCMissingCount": 0.0 }, { "institution": "Tangerine", "civicAddressMissingCount": 7.0, "totalRequests": 409.0, "cityMissingCount": 25.0, "provinceMissingCount": 25.0, "countryMissingCount": 49.0, "postalCodeMissingCount": 25.0, "partialKYCMissingCount": 49.0 }, { "institution": "Laurentienne", "civicAddressMissingCount": 0.0, "totalRequests": 30.0, "cityMissingCount": 0.0, "provinceMissingCount": 0.0, "countryMissingCount": 4.0, "postalCodeMissingCount": 0.0, "partialKYCMissingCount": 4.0 }, { "institution": "249570", "civicAddressMissingCount": 0.0, "totalRequests": 0.0, "cityMissingCount": 0.0, "provinceMissingCount": 0.0, "countryMissingCount": 0.0, "postalCodeMissingCount": 0.0, "partialKYCMissingCount": 0.0 }, { "institution": "23930", "civicAddressMissingCount": 0.0, "totalRequests": 0.0, "cityMissingCount": 0.0, "provinceMissingCount": 0.0, "countryMissingCount": 0.0, "postalCodeMissingCount": 0.0, "partialKYCMissingCount": 0.0 }, { "institution": "412410", "civicAddressMissingCount": 0.0, "totalRequests": 0.0, "cityMissingCount": 0.0, "provinceMissingCount": 0.0, "countryMissingCount": 0.0, "postalCodeMissingCount": 0.0, "partialKYCMissingCount": 0.0 }, { "institution": "BMO", "civicAddressMissingCount": 652.0, "totalRequests": 2033.0, "cityMissingCount": 652.0, "provinceMissingCount": 652.0, "countryMissingCount": 675.0, "postalCodeMissingCount": 652.0, "partialKYCMissingCount": 675.0 }, { "institution": "263580", "civicAddressMissingCount": 0.0, "totalRequests": 0.0, "cityMissingCount": 0.0, "provinceMissingCount": 0.0, "countryMissingCount": 0.0, "postalCodeMissingCount": 0.0, "partialKYCMissingCount": 0.0 }, { "institution": "PC", "civicAddressMissingCount": 0.0, "totalRequests": 24.0, "cityMissingCount": 0.0, "provinceMissingCount": 0.0, "countryMissingCount": 0.0, "postalCodeMissingCount": 0.0, "partialKYCMissingCount": 0.0 }, { "institution": "Meridian", "civicAddressMissingCount": 0.0, "totalRequests": 50.0, "cityMissingCount": 0.0, "provinceMissingCount": 0.0, "countryMissingCount": 2.0, "postalCodeMissingCount": 0.0, "partialKYCMissingCount": 2.0 }, { "institution": "412090", "civicAddressMissingCount": 0.0, "totalRequests": 0.0, "cityMissingCount": 0.0, "provinceMissingCount": 0.0, "countryMissingCount": 0.0, "postalCodeMissingCount": 0.0, "partialKYCMissingCount": 0.0 }, { "institution": "52970", "civicAddressMissingCount": 0.0, "totalRequests": 0.0, "cityMissingCount": 0.0, "provinceMissingCount": 0.0, "countryMissingCount": 0.0, "postalCodeMissingCount": 0.0, "partialKYCMissingCount": 0.0 }, { "institution": "EQBank", "civicAddressMissingCount": 3.0, "totalRequests": 52.0, "cityMissingCount": 3.0, "provinceMissingCount": 3.0, "countryMissingCount": 8.0, "postalCodeMissingCount": 3.0, "partialKYCMissingCount": 8.0 }, { "institution": "60", "civicAddressMissingCount": 0.0, "totalRequests": 4.0, "cityMissingCount": 4.0, "provinceMissingCount": 4.0, "countryMissingCount": 4.0, "postalCodeMissingCount": 4.0, "partialKYCMissingCount": 4.0 }, { "institution": "259640", "civicAddressMissingCount": 0.0, "totalRequests": 0.0, "cityMissingCount": 0.0, "provinceMissingCount": 0.0, "countryMissingCount": 0.0, "postalCodeMissingCount": 0.0, "partialKYCMissingCount": 0.0 }, { "institution": "HSBC", "civicAddressMissingCount": 63.0, "totalRequests": 63.0, "cityMissingCount": 63.0, "provinceMissingCount": 63.0, "countryMissingCount": 63.0, "postalCodeMissingCount": 63.0, "partialKYCMissingCount": 63.0 }, { "institution": "0", "civicAddressMissingCount": 0.0, "totalRequests": 0.0, "cityMissingCount": 0.0, "provinceMissingCount": 0.0, "countryMissingCount": 0.0, "postalCodeMissingCount": 0.0, "partialKYCMissingCount": 0.0 }, { "institution": "Scotia", "civicAddressMissingCount": 14.0, "totalRequests": 1256.0, "cityMissingCount": 17.0, "provinceMissingCount": 17.0, "countryMissingCount": 35.0, "postalCodeMissingCount": 17.0, "partialKYCMissingCount": 35.0 }, { "institution": "24640", "civicAddressMissingCount": 0.0, "totalRequests": 0.0, "cityMissingCount": 0.0, "provinceMissingCount": 0.0, "countryMissingCount": 0.0, "postalCodeMissingCount": 0.0, "partialKYCMissingCount": 0.0 }, { "institution": "19", "civicAddressMissingCount": 22.0, "totalRequests": 101.0, "cityMissingCount": 75.0, "provinceMissingCount": 70.0, "countryMissingCount": 73.0, "postalCodeMissingCount": 70.0, "partialKYCMissingCount": 75.0 }, { "institution": "100", "civicAddressMissingCount": 0.0, "totalRequests": 0.0, "cityMissingCount": 0.0, "provinceMissingCount": 0.0, "countryMissingCount": 0.0, "postalCodeMissingCount": 0.0, "partialKYCMissingCount": 0.0 }, { "institution": "Desjardins", "civicAddressMissingCount": 10.0, "totalRequests": 785.0, "cityMissingCount": 22.0, "provinceMissingCount": 25.0, "countryMissingCount": 93.0, "postalCodeMissingCount": 47.0, "partialKYCMissingCount": 125.0 }, { "institution": "29860", "civicAddressMissingCount": 0.0, "totalRequests": 0.0, "cityMissingCount": 0.0, "provinceMissingCount": 0.0, "countryMissingCount": 0.0, "postalCodeMissingCount": 0.0, "partialKYCMissingCount": 0.0 }, { "institution": "RBC", "civicAddressMissingCount": 9.0, "totalRequests": 1607.0, "cityMissingCount": 21.0, "provinceMissingCount": 38.0, "countryMissingCount": 129.0, "postalCodeMissingCount": 21.0, "partialKYCMissingCount": 129.0 }, { "institution": "136810", "civicAddressMissingCount": 0.0, "totalRequests": 0.0, "cityMissingCount": 0.0, "provinceMissingCount": 0.0, "countryMissingCount": 0.0, "postalCodeMissingCount": 0.0, "partialKYCMissingCount": 0.0 }, { "institution": "7730", "civicAddressMissingCount": 0.0, "totalRequests": 0.0, "cityMissingCount": 0.0, "provinceMissingCount": 0.0, "countryMissingCount": 0.0, "postalCodeMissingCount": 0.0, "partialKYCMissingCount": 0.0 }, { "institution": "114950", "civicAddressMissingCount": 0.0, "totalRequests": 0.0, "cityMissingCount": 0.0, "provinceMissingCount": 0.0, "countryMissingCount": 0.0, "postalCodeMissingCount": 0.0, "partialKYCMissingCount": 0.0 }, { "institution": "117030", "civicAddressMissingCount": 0.0, "totalRequests": 0.0, "cityMissingCount": 0.0, "provinceMissingCount": 0.0, "countryMissingCount": 0.0, "postalCodeMissingCount": 0.0, "partialKYCMissingCount": 0.0 }, { "institution": "20", "civicAddressMissingCount": 0.0, "totalRequests": 0.0, "cityMissingCount": 0.0, "provinceMissingCount": 0.0, "countryMissingCount": 0.0, "postalCodeMissingCount": 0.0, "partialKYCMissingCount": 0.0 }, { "institution": "CIBC", "civicAddressMissingCount": 21.0, "totalRequests": 1411.0, "cityMissingCount": 21.0, "provinceMissingCount": 24.0, "countryMissingCount": 59.0, "postalCodeMissingCount": 22.0, "partialKYCMissingCount": 59.0 }, { "institution": "CoastCapital", "civicAddressMissingCount": 4.0, "totalRequests": 30.0, "cityMissingCount": 4.0, "provinceMissingCount": 4.0, "countryMissingCount": 4.0, "postalCodeMissingCount": 4.0, "partialKYCMissingCount": 4.0 }, { "institution": "229060", "civicAddressMissingCount": 0.0, "totalRequests": 0.0, "cityMissingCount": 0.0, "provinceMissingCount": 0.0, "countryMissingCount": 0.0, "postalCodeMissingCount": 0.0, "partialKYCMissingCount": 0.0 }, { "institution": "ATB", "civicAddressMissingCount": 1.0, "totalRequests": 157.0, "cityMissingCount": 6.0, "provinceMissingCount": 6.0, "countryMissingCount": 8.0, "postalCodeMissingCount": 6.0, "partialKYCMissingCount": 8.0 }, { "institution": "202670", "civicAddressMissingCount": 0.0, "totalRequests": 0.0, "cityMissingCount": 0.0, "provinceMissingCount": 0.0, "countryMissingCount": 0.0, "postalCodeMissingCount": 0.0, "partialKYCMissingCount": 0.0 }, { "institution": "National", "civicAddressMissingCount": 7.0, "totalRequests": 254.0, "cityMissingCount": 9.0, "provinceMissingCount": 9.0, "countryMissingCount": 22.0, "postalCodeMissingCount": 9.0, "partialKYCMissingCount": 22.0 }, { "institution": "11070", "civicAddressMissingCount": 0.0, "totalRequests": 0.0, "cityMissingCount": 0.0, "provinceMissingCount": 0.0, "countryMissingCount": 0.0, "postalCodeMissingCount": 0.0, "partialKYCMissingCount": 0.0 }, { "institution": "19600", "civicAddressMissingCount": 0.0, "totalRequests": 0.0, "cityMissingCount": 0.0, "provinceMissingCount": 0.0, "countryMissingCount": 0.0, "postalCodeMissingCount": 0.0, "partialKYCMissingCount": 0.0 }, { "institution": "263650", "civicAddressMissingCount": 0.0, "totalRequests": 0.0, "cityMissingCount": 0.0, "provinceMissingCount": 0.0, "countryMissingCount": 0.0, "postalCodeMissingCount": 0.0, "partialKYCMissingCount": 0.0 }, { "institution": "Simplii", "civicAddressMissingCount": 3.0, "totalRequests": 265.0, "cityMissingCount": 3.0, "provinceMissingCount": 3.0, "countryMissingCount": 6.0, "postalCodeMissingCount": 3.0, "partialKYCMissingCount": 6.0 }, { "institution": "209350", "civicAddressMissingCount": 0.0, "totalRequests": 0.0, "cityMissingCount": 0.0, "provinceMissingCount": 0.0, "countryMissingCount": 0.0, "postalCodeMissingCount": 0.0, "partialKYCMissingCount": 0.0 }, { "institution": "Vancity", "civicAddressMissingCount": 0.0, "totalRequests": 83.0, "cityMissingCount": 0.0, "provinceMissingCount": 0.0, "countryMissingCount": 1.0, "postalCodeMissingCount": 0.0, "partialKYCMissingCount": 1.0 }, { "institution": "5050120", "civicAddressMissingCount": 0.0, "totalRequests": 0.0, "cityMissingCount": 0.0, "provinceMissingCount": 0.0, "countryMissingCount": 0.0, "postalCodeMissingCount": 0.0, "partialKYCMissingCount": 0.0 }, { "institution": "TD", "civicAddressMissingCount": 63.0, "totalRequests": 2327.0, "cityMissingCount": 63.0, "provinceMissingCount": 65.0, "countryMissingCount": 120.0, "postalCodeMissingCount": 65.0, "partialKYCMissingCount": 120.0 }];
+
+var toDate = moment().format("YYYY-MM-DDTHH:mm");
+var fromDate = moment().subtract(1, "days").format("YYYY-MM-DDTHH:mm");
+
+$(document).ready(function() {
+    refreshTable();
+});
+
+$(function() {
+    $('#fromDate').datetimepicker({
+        format: "YYYY-MM-DDTHH:mm",
+        defaultDate: moment(new Date()).subtract(1, "days")
+    });
+
+    $('#toDate').datetimepicker({
+        format: "YYYY-MM-DDTHH:mm",
+        defaultDate: moment(new Date())
+    });
+
+    $('#fromDate').on("change.datetimepicker", function(e) {
+        console.log(e.date.format("YYYY-MM-DDTHH:mm"));
+        fromDate = e.date.format("YYYY-MM-DDTHH:mm");
+    });
+    $('#toDate').on("change.datetimepicker", function(e) {
+        toDate = e.date.format("YYYY-MM-DDTHH:mm");
+    });
+});
+
+function refreshTable() {
+    $('#kycTable').DataTable({
+        "paging": true,
+        "ordering": true,
+        "info": true,
+        "searching": true,
+        "pageLength": 25,
+        "ajax": {
+            url: ['https://requests.internal.fin.ag/api/kpi/KYCKPI?from=', fromDate, '&to=', toDate].join(''),
+            contentType: 'application/json; charset=utf-8',
+            type: 'get',
+            dataSrc: function(json) {
+                return json;
+            }
+        },
+        "columns": [
+            { "data": "institution" },
+            { "data": "totalCompletedRequests" },
+            { "data": "missingCivicAddress" },
+            { "data": "missingCity" },
+            { "data": "missingProvince" },
+            { "data": "missingCountry" },
+            { "data": "missingPostalCode" },
+            { "data": "missingHolderName" },
+            { "data": "missingEmail" },
+            { "data": "missingPartialKYC" },
+        ],
+        "bDestroy": true,
+        "columnDefs": [{
+            "targets": 1,
+            "data": "totalRequests",
+            "render": function(data, type, row, meta) {
+                //console.log(row);
+                return '<a target="_blank" href=IBVDetails.html?Institution=' + row.institution + '&field=totalRequests&from=' + fromDate + 'to=' + toDate + '>' + data + ' </a>';
+            }
+        }]
+    });
+}
+
+function RefreshTable(tableId, urlData) {
+    $.getJSON(urlData, null, function(json) {
+        table = $(tableId).dataTable();
+        oSettings = table.fnSettings();
+
+        table.fnClearTable(this);
+
+        for (var i = 0; i < json.aaData.length; i++) {
+            table.oApi._fnAddData(oSettings, json.aaData[i]);
+        }
+
+        oSettings.aiDisplay = oSettings.aiDisplayMaster.slice();
+        table.fnDraw();
+    });
+}
+
+function loadNewData() {
+    var url = ['https://requests.internal.fin.ag/api/kpi/KYCKPI?from=', fromDate, '&to=', toDate].join('');
+    console.log(url);
+    $('#kycTable').DataTable(url).ajax.load();
+    //RefreshTable("#kycTable", url);
+}
